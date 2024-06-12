75a11c1cb99f1b4e7753f08b455d3a82fe615365
Evgeniy Chernyshenko <yevgeniyche@gmail.com>
2024-06-12 17:04:05 +0300

    Добавить моковый сервер

---
+++ server/index.js
@@ -0,0 +1,57 @@
+const jsonServer = require("json-server");
+const path = require("path");
+const fs = require("fs/promises");
+
+const DB_FILE_PATH = path.resolve(__dirname, "db.json");
+const RESPONSE_DELAY = 500;
+const SERVER_PORT = 8000;
+
+const server = jsonServer.create();
+const router = jsonServer.router(DB_FILE_PATH);
+
+server.use(jsonServer.defaults());
+server.use(jsonServer.bodyParser);
+
+server.use((req, res, next) => {
+  setTimeout(() => {
+    next();
+  }, RESPONSE_DELAY);
+});
+
+server.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const db = JSON.parse(await fs.readFile(DB_FILE_PATH));
+
+    const userFromDb = db.users?.find(
+      (user) => user.username === username && user.password === password,
+    );
+
+    if (!userFromDb) {
+      res.status(403).json({ message: "User not found" });
+
+      return;
+    }
+
+    res.json(userFromDb);
+  } catch (error) {
+    res.status(500).json({ message: error.message ?? "Internal server error" });
+  }
+});
+
+server.use((req, res, next) => {
+  if (!req.headers.authorization) {
+    res.status(403).json({ message: "AUTH ERROR" });
+
+    return;
+  }
+
+  next();
+});
+
+server.use(router);
+
+server.listen(SERVER_PORT, () => {
+  console.info(`Server is running on port ${SERVER_PORT}`);
+});
